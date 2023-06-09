4f277fc5b08221d2fe2e77c606db843b7b945666
unknown <sculptoid11@gmail.com>
2023-06-09 16:47:18 -0400

    Continued work on Thoughts Route

---
+++ routes/thought.js
@@ -0,0 +1,87 @@
+const { User, Thought } = require('../models');
+
+const getThoughts = async (req, res) => {
+  try {
+    const thoughts = await Thought.aggregate([
+      {
+        $lookup: {
+          from: 'thoughts',
+          localField: 'reactions',
+          foreignField: '_id',
+          as: 'reactions',
+        },
+      },
+    ]);
+
+    res.json(thoughts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getThoughtById = async (req, res) => {
+  const { thoughtId } = req.params;
+  try {
+    const thought = await Thought.aggregate([
+      {
+        $lookup: {
+          from: 'thoughts',
+          localField: 'reactions',
+          foreignField: '_id',
+          as: 'reactions',
+        },
+      },
+    ]).findById(thoughtId);
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const createThought = async (req, res) => {
+  const { username, thoughtText } = req.body;
+  try {
+    // Create the thought
+    const thought = await Thought.create({ thoughtText, username });
+
+    // Update the associated user's thoughts array
+    const user = await User.findOneAndUpdate(
+      { username },
+      { $push: { thoughts: thought._id } },
+      { new: true }
+    );
+
+    res.status(201).json({ thought, user });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const updateThought = async (req, res) => {
+  const { thoughtId } = req.params;
+  try {
+      const thought = await Thought.findByIdAndUpdate(thoughtId, req.body, { new: true });
+      if (!thought) {
+          return res.status(404).json({ message: 'Thought not found' });
+      }
+      res.json(thought);
+  } catch (err) {
+      res.status(400).json(err);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const { userId } = req.params;
+  try {
+      const user = await User.findByIdAndRemove(userId);
+      if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+      }
+      res.json({ message: 'User deleted successfully' });
+  } catch (err) {
+      res.status(400).json(err);
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -2,68 +2,113 @@ const { User } = require('../models');
 
 
 const getUsers = async (req, res) => {
-  try {
-    const users = await User.find().populate('thoughts').populate('friends');
-    res.json(users);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    try {
+        const users = await User.find().populate('thoughts').populate('friends');
+        res.json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
 const getUserById = async (req, res) => {
-  const { userId } = req.params;
-  try {
-    const user = await User.findById(userId).populate('thoughts').populate('friends');
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+    const { userId } = req.params;
+    try {
+        const user = await User.findById(userId).populate('thoughts').populate('friends');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
     }
-    res.json(user);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 };
 
 const createUser = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 };
 
 
 const updateUser = async (req, res) => {
-  const { userId } = req.params;
-  try {
-    const user = await User.findByIdAndUpdate(userId, req.body, { new: true });
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+    const { userId } = req.params;
+    try {
+        const user = await User.findByIdAndUpdate(userId, req.body, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(400).json(err);
     }
-    res.json(user);
-  } catch (err) {
-    res.status(400).json(err);
-  }
 };
 
 
 const deleteUser = async (req, res) => {
-  const { userId } = req.params;
-  try {
-    const user = await User.findByIdAndRemove(userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+    const { userId } = req.params;
+    try {
+        const user = await User.findByIdAndRemove(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json({ message: 'User deleted successfully' });
+    } catch (err) {
+        res.status(400).json(err);
     }
-    res.json({ message: 'User deleted successfully' });
-  } catch (err) {
-    res.status(400).json(err);
-  }
+};
+
+const addFriend = async (req, res) => {
+    const { userId, friendId } = req.params;
+    try {
+        const user = await User.findById(userId);
+        const friend = await User.findById(friendId);
+
+        if (!user || !friend) {
+            return res.status(404).json({ message: 'User or friend not found' });
+        }
+
+        user.friends.push(friendId);
+        await user.save();
+
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+const removeFriend = async (req, res) => {
+    const { userId, friendId } = req.params;
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const friendIndex = user.friends.indexOf(friendId);
+
+        if (friendIndex === -1) {
+            return res.status(404).json({ message: 'Friend not found in the user\'s friend list' });
+        }
+
+        user.friends.splice(friendIndex, 1);
+        await user.save();
+
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
 module.exports = {
-  getUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
 };
