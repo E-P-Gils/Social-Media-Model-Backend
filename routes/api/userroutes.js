591b6ca6f26bc6eda40f43b10e82a142720c5046
unknown <sculptoid11@gmail.com>
2023-06-12 17:32:41 -0400

    Spelling stuff

--- routes/api/userroutes.js
+++ routes/api/userroutes.js
@@ -11,7 +11,7 @@ const {
 
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userId').get(getUserById).post(updateUser).delete(deleteUser);
+router.route('/userId').get(getUserById).post(updateUser).delete(deleteUser);
 
 router.route('/friend').delete(removeFriend).post(addFriend);
 
